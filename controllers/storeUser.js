60f8ac2dbce618f7ea4d4f578acff35ffc71a87a
Carlos Gallardo <60414095+Carlos-Gallardoo@users.noreply.github.com>
2021-06-23 17:42:14 -0700

    bcryptjs

--- controllers/storeUser.js
+++ controllers/storeUser.js
@@ -2,7 +2,7 @@
 
 const User = require('../models/user');
 const path = require('path');
-const bcrypt = require('bcrypt')
+const bcrypt = require('bcryptjs')
 module.exports = (req,res) =>{
 
 let user = new User();
